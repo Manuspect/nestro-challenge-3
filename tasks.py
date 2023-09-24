0a7de5bcd91350f0b7cdf527378b16df5fa3fcbc
KirSR <kirsr.life@gmail.com>
2023-09-25 01:10:40 +0300

    fix: usd_kurs, parse_chrome, conda deps; features: add_urals, shared_directory

--- tasks.py
+++ tasks.py
@@ -9,41 +9,13 @@ import pandas as pd
 import logging
 import os
 import time
+import re
+import json
 
 
-@task
-def producer():
-    """Split Excel rows into multiple output Work Items for the next step."""
-    for item in workitems.inputs:
-        output_directory = os.environ.get("ROBOT_ARTIFACTS")
-        name = "orders.xlsx"
-        path = item.get_file(name, os.path.join(output_directory, name))
+output_directory = os.environ.get("ROBOT_ARTIFACTS")
+shared_directory = os.path.join(output_directory, "shared")
 
-        excel = Excel()
-        excel.open_workbook(path)
-        rows = excel.read_worksheet_as_table(header=True)
-
-        for row in rows:
-            payload = {
-                "Name": row["Name"],
-                "Zip": row["Zip"],
-                "Product": row["Item"],
-            }
-            workitems.outputs.create(payload)
-
-
-@task
-def consumer():
-    """Process all the produced input Work Items from the previous step."""
-    for item in workitems.inputs:
-        try:
-            name = item.payload["Name"]
-            address = item.payload["Zip"]
-            product = item.payload["Product"]
-            print(f"Processing order: {name}, {address}, {product}")
-            item.done()
-        except KeyError as err:
-            item.fail(code="MISSING_VALUE", message=str(err))
 
 @task
 def usd_kurs():
@@ -62,9 +34,9 @@ def usd_kurs():
         url = f"https://www.cbr.ru/currency_base/dynamics/?UniDbQuery.Posted=True&UniDbQuery.so=1&UniDbQuery.mode=1&UniDbQuery.date_req1={start_date}&UniDbQuery.date_req2={end_date}&UniDbQuery.VAL_NM_RQ=R01235"
         browser.open_available_browser(url)
         logging.info(f"Открыли сайт")
-
         # Дождитесь полной загрузки страницы
-        browser.wait_until_element_is_visible("//*[@id='content']/div/div/div/div[2]/div[1]/table/tbody/tr[1]")
+        browser.wait_until_element_is_visible(
+            "//*[@id='content']/div/div/div/div[2]/div[1]/table/tbody/tr[1]", 15)
 
         # Создаем пустой список для хранения данных
         data = []
@@ -75,25 +47,29 @@ def usd_kurs():
         # Итерируемся по дням от 239 до 1
         for i in range(239, 0, -1):
             # Получаем текст из ячейки таблицы
-            row = browser.get_text(f'//*[@id="content"]/div/div/div/div[2]/div[1]/table/tbody/tr[{i}]')
+            row = browser.get_text(
+                f'//*[@id="content"]/div/div/div/div[2]/div[1]/table/tbody/tr[{i}]')
             date = row[:10]
             rate = row[12:]
             month = date[3:5]
             logging.info(f"Текущий месяц: {month}")
-
+            rate = rate.replace(',', '.')
             # Добавляем день и его курс в общий список
-            data.append([date, float(rate.replace(',', '.'))])
+            try:
+                data.append([date, float(rate)])
 
-            # Добавляем день в соответствующий квартал
-            day_number = int(date[:2])
-            if 1 <= day_number <= 31:
-                quarters[1].append(float(rate.replace(',', '.')))
-            elif 32 <= day_number <= 61:
-                quarters[2].append(float(rate.replace(',', '.')))
-            elif 62 <= day_number <= 92:
-                quarters[3].append(float(rate.replace(',', '.')))
-            elif 93 <= day_number <= 123:
-                quarters[4].append(float(rate.replace(',', '.')))
+                # Добавляем день в соответствующий квартал
+                day_number = int(date[:2])
+                if 1 <= day_number <= 31:
+                    quarters[1].append(float(rate))
+                elif 32 <= day_number <= 61:
+                    quarters[2].append(float(rate))
+                elif 62 <= day_number <= 92:
+                    quarters[3].append(float(rate))
+                elif 93 <= day_number <= 123:
+                    quarters[4].append(float(rate))
+            except ValueError:
+                logging.info(f"Not a float: {month}")
 
         browser.close_browser()
 
@@ -114,7 +90,8 @@ def usd_kurs():
                 avg_rate = sum(days) / len(days)
                 quarterly_avg[f"Квартал {quarter}"] = [avg_rate]
 
-        quarterly_avg_df = pd.DataFrame.from_dict(quarterly_avg, orient='index', columns=["Средний курс"])
+        quarterly_avg_df = pd.DataFrame.from_dict(
+            quarterly_avg, orient='index', columns=["Средний курс"])
 
         # Сохраняем результаты в Excel файлы
         monthly_avg.to_excel("monthly_exchange_rate.xlsx", index=False)
@@ -123,6 +100,9 @@ def usd_kurs():
     except Exception as e:
         print(f"Произошла ошибка: {str(e)}")
         logging.error(f"Произошла ошибка: {str(e)}")
+    finally:
+        # Закрываем браузер после использования
+        browser.close_browser()
 
 
 @task
@@ -136,8 +116,9 @@ def parse_chrome():
         browser.input_text("name=q", f"цена нефти urals {params_chrome}")
         browser.press_keys("name=q", Keys.ENTER)
 
+        browser.wait_until_element_is_visible("//*[@alt='Google']", 15)
+
         # Ждем, пока страница загрузится
-        time.sleep(60)
         page_source = browser.get_source()
 
         # Используем BeautifulSoup для парсинга страницы
@@ -151,7 +132,7 @@ def parse_chrome():
 
         # Получаем текст из элемента с ценой и выводим его
         price = price_element.get_text()
-        print("Цена нефти Urals за сентябрь 2022 года:", price)
+        logging.info(f"Цена нефти Urals за сентябрь 2022 года: {price}")
 
         # Создаем новый файл Excel
         wb = Workbook()
@@ -180,6 +161,50 @@ def parse_chrome():
     finally:
         # Закрываем браузер после использования
         browser.close_browser()
+
+
+@task
+def add_urals():
+    parser = Selenium()
+    urls = {
+        'декабрь 2021': 'https://www.economy.gov.ru/material/directions/vneshneekonomicheskaya_deyatelnost/tamozhenno_tarifnoe_regulirovanie/o_vyvoznyh_tamozhennyh_poshlinah_na_neft_i_otdelnye_kategorii_tovarov_vyrabotannyh_iz_nefti_na_period_s_1_po_31_yanvarya_2022_goda.html',
+        'январь': 'https://www.economy.gov.ru/material/directions/vneshneekonomicheskaya_deyatelnost/tamozhenno_tarifnoe_regulirovanie/o_vyvoznyh_tamozhennyh_poshlinah_na_neft_i_otdelnye_kategorii_tovarov_vyrabotannyh_iz_nefti_na_period_s_1_po_28_fevralya_2022_goda.html',
+        'февраль': 'https://www.economy.gov.ru/material/directions/vneshneekonomicheskaya_deyatelnost/tamozhenno_tarifnoe_regulirovanie/o_vyvoznyh_tamozhennyh_poshlinah_na_neft_i_otdelnye_kategorii_tovarov_vyrabotannyh_iz_nefti_na_period_s_1_po_31_marta_2022_goda.html',
+        'март': 'https://www.economy.gov.ru/material/directions/vneshneekonomicheskaya_deyatelnost/tamozhenno_tarifnoe_regulirovanie/o_vyvoznyh_tamozhennyh_poshlinah_na_neft_i_otdelnye_kategorii_tovarov_vyrabotannyh_iz_nefti_na_period_s_1_po_30_aprelya_2022_goda.html',
+        'апрель': 'https://www.economy.gov.ru/material/directions/vneshneekonomicheskaya_deyatelnost/tamozhenno_tarifnoe_regulirovanie/o_vyvoznyh_tamozhennyh_poshlinah_na_neft_i_otdelnye_kategorii_tovarov_vyrabotannyh_iz_nefti_na_period_s_1_po_31_maya_2022_goda.html',
+        'май': 'https://www.economy.gov.ru/material/directions/vneshneekonomicheskaya_deyatelnost/tamozhenno_tarifnoe_regulirovanie/o_vyvoznyh_tamozhennyh_poshlinah_na_neft_i_otdelnye_kategorii_tovarov_vyrabotannyh_iz_nefti_na_period_s_1_po_30_iyunya_2022_goda.html',
+        'июнь': 'https://www.economy.gov.ru/material/directions/vneshneekonomicheskaya_deyatelnost/tamozhenno_tarifnoe_regulirovanie/o_vyvoznyh_tamozhennyh_poshlinah_na_neft_i_otdelnye_kategorii_tovarov_vyrabotannyh_iz_nefti_na_period_s_1_po_31_iyulya_2022_goda.html',
+        'июль': 'https://www.economy.gov.ru/material/directions/vneshneekonomicheskaya_deyatelnost/tamozhenno_tarifnoe_regulirovanie/o_vyvoznyh_tamozhennyh_poshlinah_na_neft_i_otdelnye_kategorii_tovarov_vyrabotannyh_iz_nefti_na_period_s_1_po_31_avgusta_2022_goda.html',
+        'август': 'https://www.economy.gov.ru/material/directions/vneshneekonomicheskaya_deyatelnost/tamozhenno_tarifnoe_regulirovanie/o_vyvoznyh_tamozhennyh_poshlinah_na_neft_i_otdelnye_kategorii_tovarov_vyrabotannyh_iz_nefti_na_period_s_1_po_30_sentyabrya_2022_goda.html',
+        'сентябрь': 'https://www.economy.gov.ru/material/directions/vneshneekonomicheskaya_deyatelnost/tamozhenno_tarifnoe_regulirovanie/o_vyvoznyh_tamozhennyh_poshlinah_na_neft_i_otdelnye_kategorii_tovarov_vyrabotannyh_iz_nefti_na_period_s_1_po_31_oktyabrya_2022_goda.html',
+        'октябрь': 'https://www.economy.gov.ru/material/directions/vneshneekonomicheskaya_deyatelnost/tamozhenno_tarifnoe_regulirovanie/o_vyvoznyh_tamozhennyh_poshlinah_na_neft_i_otdelnye_kategorii_tovarov_vyrabotannyh_iz_nefti_na_period_s_1_po_30_noyabrya_2022_goda.html',
+        'ноябрь': 'https://www.economy.gov.ru/material/directions/vneshneekonomicheskaya_deyatelnost/tamozhenno_tarifnoe_regulirovanie/o_vyvoznyh_tamozhennyh_poshlinah_na_neft_i_otdelnye_kategorii_tovarov_vyrabotannyh_iz_nefti_na_period_s_1_po_31_dekabrya_2022_goda.html',
+        'декабрь': 'https://www.economy.gov.ru/material/directions/vneshneekonomicheskaya_deyatelnost/tamozhenno_tarifnoe_regulirovanie/o_vyvoznyh_tamozhennyh_poshlinah_na_neft_i_otdelnye_kategorii_tovarov_vyrabotannyh_iz_nefti_na_period_s_1_po_31_yanvarya_2023_goda.html'
+    }
+    payload = {}
+    urals_payload = {}
+    tax_rates = {}
+    keys = list(urls.keys())
+    for index, month in enumerate(keys):
+        parser.open_available_browser(urls[month])
+        if index != 0:
+            urals = parser.get_text("xpath=//table/tbody/tr[2]/td/p")
+            urals = urals.replace(',', '.')
+            urals_payload[month] = float(urals.split(' ')[0])
+
+        if index != (len(urls)-1):
+            tax_rate = parser.get_text("xpath=//table/tbody/tr/td[3]/p")
+            tax_rate = tax_rate.replace(',', '.')
+            tax_rates[keys[index+1]] = float(tax_rate)
+        parser.close_browser()
+    payload = {
+        "urals_payload": urals_payload,
+        "tax_rates": tax_rates
+    }
+    # workitems.outputs.create(urals_payload)
+    with open(os.path.join(shared_directory, 'workitems.json'), "w") as outfile:
+        json.dump(payload, outfile)
+
 
 @task
 def pars_exel():
@@ -232,4 +257,3 @@ def pars_exel():
 
     # Сохраните обновленные данные в Приложение_1.xlsx
     app1_data.to_excel('Приложение_1.xlsx', index=False)
-
